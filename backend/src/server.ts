20ec4c7a53375fb77f05cb7ec59a2566942385dc
osas706 <osas.omoregie@sprinble.com>
2026-04-05 07:28:34 +0100

    MongoDB setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express, { type Request, type Response } from "express";
 import { ENV } from "./lib/env.ts";
-import path from "path";
+// import path from "path";
+import { connectDB } from "./lib/db.ts";
+import console from "console";
 
 const app = express();
 
@@ -18,6 +20,16 @@ app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({message: "Sucesss, Api running"})
 });
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server running on http://localhost:${ENV.PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server running on http://localhost:${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server", error)
+  };
+};
+
+startServer();
